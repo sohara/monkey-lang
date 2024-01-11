bf8de564cb6f9c4fe1fa18b804d10694f8d0f128
Sean O'Hara <sohara@sohara.com>
2024-01-11 15:58:05 -0500

    Cleanup tests and add some helpers

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -235,92 +235,13 @@ func TestParsingInfixExpressions(t *testing.T) {
 				program.Statements[0])
 		}
 
-		exp, ok := stmt.Expression.(*ast.InfixExpression)
-		if !ok {
-			t.Fatalf("exp is not ast.InfixExpression. got=%T", stmt.Expression)
-		}
-
-		if !testIntegerLiteral(t, exp.Left, tt.leftValue) {
+		if !testInfixExpression(t, stmt.Expression, tt.leftValue,
+			tt.operator, tt.rightValue) {
 			return
 		}
 
-		if exp.Operator != tt.operator {
-			t.Fatalf("exp.Operator is not '%s'. got=%s",
-				tt.operator, exp.Operator)
-		}
-
-		if !testIntegerLiteral(t, exp.Right, tt.rightValue) {
-			return
-		}
 	}
 }
-
-// func TestOperatorPrecedenceParsing(t *testing.T) {
-// 	tests := []struct {
-// 		input    string
-// 		expected string
-// 	}{
-// 		{
-// 			"-a * b",
-// 			"((-a) * b)",
-// 		},
-// 		{
-// 			"!-a",
-// 			"(!(-a))",
-// 		},
-// 		{
-// 			"a + b + c",
-// 			"((a + b) + c)",
-// 		},
-// 		{
-// 			"a + b - c",
-// 			"((a + b) - c)",
-// 		},
-// 		{
-// 			"a * b * c",
-// 			"((a * b) * c)",
-// 		},
-// 		{
-// 			"a * b / c",
-// 			"((a * b) / c)",
-// 		},
-// 		{
-// 			"a + b / c",
-// 			"(a + (b / c))",
-// 		},
-// 		{
-// 			"a + b * c + d / e - f",
-// 			"(((a + (b * c)) + (d / e)) - f)",
-// 		},
-// 		{
-// 			"3 + 4; -5 * 5",
-// 			"(3 + 4)((-5) * 5)",
-// 		},
-// 		// {
-// 		// 	"5 > 4 == 3 < 4",
-// 		// 	"((5 > 4) == (3 < 4))",
-// 		// },
-// 		// {
-// 		// 	"5 < 4 != 3 > 4",
-// 		// 	"((5 < 4) != (3 > 4))",
-// 		// },
-// 		// {
-// 		// 	"3 + 4 * 5 == 3 * 1 + 4 * 5",
-// 		// 	"((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
-// 		// },
-// 	}
-//
-// 	for _, tt := range tests {
-// 		l := lexer.New(tt.input)
-// 		p := New(l)
-// 		program := p.ParseProgram()
-// 		checkParserErrors(t, p)
-// 		actual := program.String()
-// 		if actual != tt.expected {
-// 			t.Errorf("expect=%q, got=%q", tt.expected, actual)
-// 		}
-// 	}
-// }
 
 func TestOperatorPrecedenceParsing(t *testing.T) {
 	tests := []struct {
@@ -455,6 +376,64 @@ func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
 
 	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
 		t.Errorf("integ.TokenLiteral not %d. got=%s", value, integ.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
+func testIdentifer(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Identifier. got=%T", exp)
+		return false
+	}
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifer(t, exp, v)
+	}
+	t.Errorf("type of exp not handled. got=%T(%s)", exp, exp)
+	return false
+}
+
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{},
+	operator string, right interface{},
+) bool {
+	opExp, ok := exp.(*ast.InfixExpression)
+
+	if !ok {
+		t.Errorf("exp is not ast.InfixExpression. got=%T(%s)", exp, exp)
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Left, left) {
+		return false
+	}
+
+	if opExp.Operator != operator {
+		t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Right, right) {
 		return false
 	}
 
