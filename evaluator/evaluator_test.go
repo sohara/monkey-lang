049cb31699be4383742767b0ac5418e501bc613a
Sean O'Hara <sohara@sohara.com>
2024-01-12 21:50:27 -0500

    Add evaluator with support for ints and bools

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,92 @@
+package evaluator
+
+import (
+	"monkey-lang/lexer"
+	"monkey-lang/object"
+	"monkey-lang/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+		// {"1 < 2", true},
+		// {"1 > 2", false},
+		// {"1 < 1", false},
+		// {"1 > 1", false},
+		// {"1 == 1", true},
+		// {"1 != 1", false},
+		// {"1 == 2", false},
+		// {"1 != 2", true},
+		// {"true == true", true},
+		// {"false == false", true},
+		// {"true == false", false},
+		// {"true != false", true},
+		// {"false != true", true},
+		// {"(1 < 2) == true", true},
+		// {"(1 < 2) == false", false},
+		// {"(1 > 2) == true", false},
+		// {"(1 > 2) == false", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
