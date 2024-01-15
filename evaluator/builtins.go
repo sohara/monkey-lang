6b811e8b55b9df8f95fddb0d34c8941a6e7c9925
Sean O'Hara <sohara@sohara.com>
2024-01-15 09:19:21 -0500

    Add push array builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -60,7 +60,7 @@ var builtins = map[string]*object.Builtin{
 					len(args))
 			}
 			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `last` must be ARRAY, got %s",
+				return newError("argument to `rest` must be ARRAY, got %s",
 					args[0].Type())
 			}
 			arr := args[0].(*object.Array)
@@ -71,6 +71,26 @@ var builtins = map[string]*object.Builtin{
 				return &object.Array{Elements: newElements}
 			}
 			return NULL
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `push` must be ARRAY, got %s",
+					args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			length := len(arr.Elements)
+
+			newElements := make([]object.Object, length+1, length+1)
+			copy(newElements, arr.Elements)
+			newElements[length] = args[1]
+			return &object.Array{Elements: newElements}
 		},
 	},
 }
