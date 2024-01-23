af8067c4e314541c314f76367b97742f14a9503f
Sean O'Hara <sohara@sohara.com>
2024-01-23 15:55:39 -0500

    Support HashLiterals

--- code/code.go
+++ code/code.go
@@ -70,6 +70,7 @@ const (
 	OpGetGlobal
 	OpSetGlobal
 	OpArray
+	OpHash
 )
 
 type Definition struct {
@@ -97,6 +98,7 @@ var definitions = map[Opcode]*Definition{
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
