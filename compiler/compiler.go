3833f8f6433518236a2b04444621c72d135fe67e
Sean O'Hara <sohara@sohara.com>
2024-01-24 15:51:47 -0500

    Compiler call expressions

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -260,6 +260,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		compiledFn := &object.CompiledFunction{Instructions: instructions}
 		c.emit(code.OpConstant, c.addConstant(compiledFn))
 
+	case *ast.CallExpression:
+		err := c.Compile(node.Function)
+		if err != nil {
+			return err
+		}
+
+		c.emit(code.OpCall)
+
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
 		if err != nil {
