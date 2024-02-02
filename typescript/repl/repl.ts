773520866f3611924b4bc82a42c52cd578acbfa9
Sean O'Hara <sohara@sohara.com>
2024-02-02 11:45:24 -0500

    Use parser in REPL (just show parsed program.string)

--- typescript/repl/repl.ts
+++ typescript/repl/repl.ts
@@ -1,6 +1,7 @@
 import * as readline from "readline";
 import { Lexer } from "../lexer/lexer";
 import { TokenType } from "../token/token";
+import { Parser } from "../parser/parser";
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -12,13 +13,14 @@ const PROMPT = ">> ";
 export function start() {
   rl.on("line", (input) => {
     const lexer = new Lexer(input);
-    for (
-      let tok = lexer.nextToken();
-      tok.type !== TokenType.EOF;
-      tok = lexer.nextToken()
-    ) {
-      console.log(tok);
+    const parser = new Parser(lexer);
+    const program = parser.parseProgram();
+
+    if (parser.errors.length > 0) {
+      printParseErrors(parser.errors);
     }
+
+    console.log(program.string);
     rl.prompt();
   }).on("close", () => {
     console.log("REPL terminated");
@@ -27,4 +29,26 @@ export function start() {
 
   rl.setPrompt(PROMPT);
   rl.prompt();
+}
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`;
+
+function printParseErrors(errors: string[]) {
+  console.log(MONKEY_FACE);
+  console.log("Whoops! We ran into some monkey business here!");
+  console.log("Parser errors:");
+  for (const error of errors) {
+    console.log(error);
+  }
 }
