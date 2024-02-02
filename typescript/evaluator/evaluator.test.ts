6efc432b8522f3dc51755e0eb6f50514a1c7085a
Sean O'Hara <sohara@sohara.com>
2024-02-02 15:51:34 -0500

    Support infix and boolean integer expressions

--- typescript/evaluator/evaluator.test.ts
+++ typescript/evaluator/evaluator.test.ts
@@ -9,6 +9,19 @@ test("evaluation of integer expressions", () => {
   const tests: [string, number][] = [
     ["5", 5],
     ["10", 10],
+    ["-5", -5],
+    ["-10", -10],
+    ["5 + 5 + 5 + 5 - 10", 10],
+    ["2 * 2 * 2 * 2 * 2", 32],
+    ["-50 + 100 + -50", 0],
+    ["5 * 2 + 10", 20],
+    ["5 + 2 * 10", 25],
+    ["20 + 2 * -10", 0],
+    ["50 / 2 * 2 + 10", 60],
+    ["2 * (5 + 10)", 30],
+    ["3 * 3 * 3 + 10", 37],
+    ["3 * (3 * 3) + 10", 37],
+    ["(5 + 10 * 2 + 15 / 3) * 2 + -10", 50],
   ];
 
   for (const [input, expected] of tests) {
@@ -21,6 +34,14 @@ test("evaluation of boolean expressions", () => {
   const tests: [string, boolean][] = [
     ["true", true],
     ["false", false],
+    ["1 < 2", true],
+    ["1 > 2", false],
+    ["1 < 1", false],
+    ["1 > 1", false],
+    ["1 == 1", true],
+    ["1 != 1", false],
+    ["1 == 2", false],
+    ["1 != 2", true],
   ];
   for (const [input, expected] of tests) {
     const evaluated = testEval(input);
--- typescript/evaluator/evaluator.ts
+++ typescript/evaluator/evaluator.ts
@@ -1,13 +1,20 @@
 import type {
   BooleanLiteral,
   ExpressionStatement,
+  InfixExpression,
   IntegerLiteral,
   Node,
   PrefixExpression,
   Program,
   Statement,
 } from "../ast/ast";
-import { BooleanObj, Integer, NullObj, type Obj } from "../object/object";
+import {
+  BooleanObj,
+  INTEGER_OBJ,
+  Integer,
+  NullObj,
+  type Obj,
+} from "../object/object";
 
 const TRUE = new BooleanObj(true);
 const FALSE = new BooleanObj(false);
@@ -16,28 +23,95 @@ const NULL = new NullObj();
 export function evaluate(node: Node): Obj | null {
   switch (node.constructor.name) {
     // Statements
-    case "Program":
+    case "Program": {
       return evalStatements((node as Program).statements);
-    case "ExpressionStatement":
+    }
+    case "ExpressionStatement": {
       return evaluate((node as ExpressionStatement).expression);
+    }
 
     // Expressions
-    case "IntegerLiteral":
+    case "IntegerLiteral": {
       return new Integer((node as IntegerLiteral).value);
-    case "BooleanLiteral":
+    }
+    case "BooleanLiteral": {
       return nativeBoolToBooleanObject((node as BooleanLiteral).value);
-    case "PrefixExpression":
+    }
+    case "PrefixExpression": {
       const right = evaluate((node as PrefixExpression).right);
       return evalPrefixExpression((node as PrefixExpression).operator, right);
+    }
+    case "InfixExpression": {
+      const left = evaluate((node as InfixExpression).left);
+      const right = evaluate((node as InfixExpression).right);
+      if (!right || !left) {
+        return NULL;
+      }
+      return evalInfixExpression(
+        (node as InfixExpression).operator,
+        left,
+        right,
+      );
+    }
   }
 
   return null;
+}
+
+function evalInfixExpression(
+  operator: string,
+  left: Obj,
+  right: Obj,
+): Obj | null {
+  switch (true) {
+    case left.type === INTEGER_OBJ && right.type === INTEGER_OBJ: {
+      return evalIntegerInfixExpression(
+        operator,
+        left as Integer,
+        right as Integer,
+      );
+    }
+    default: {
+      return NULL;
+    }
+  }
 }
 
 function evalPrefixExpression(operator: string, right: Obj | null): Obj {
   switch (operator) {
     case "!":
       return evalBangOperatorExpression(right);
+    case "-":
+      return evalMinusePrefixExpression(right);
+    default:
+      return NULL;
+  }
+}
+
+function evalIntegerInfixExpression(
+  operator: string,
+  left: Integer,
+  right: Integer,
+): Obj {
+  const leftVal = left.value;
+  const rightVal = right.value;
+  switch (operator) {
+    case "+":
+      return new Integer(leftVal + rightVal);
+    case "-":
+      return new Integer(leftVal - rightVal);
+    case "*":
+      return new Integer(leftVal * rightVal);
+    case "/":
+      return new Integer(leftVal / rightVal);
+    case "<":
+      return nativeBoolToBooleanObject(leftVal < rightVal);
+    case ">":
+      return nativeBoolToBooleanObject(leftVal > rightVal);
+    case "==":
+      return nativeBoolToBooleanObject(leftVal === rightVal);
+    case "!=":
+      return nativeBoolToBooleanObject(leftVal !== rightVal);
     default:
       return NULL;
   }
@@ -54,6 +128,14 @@ function evalBangOperatorExpression(right: Obj | null): Obj {
     default:
       return FALSE;
   }
+}
+
+function evalMinusePrefixExpression(right: Obj | null): Obj {
+  if (!right || right?.type !== INTEGER_OBJ) {
+    return NULL;
+  }
+  const value = (right as Integer).value;
+  return new Integer(-value);
 }
 
 function evalStatements(statements: Statement[]): Obj | null {
